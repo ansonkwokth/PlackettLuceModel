c3a64b4a30a99f2ba464d6dea661d1ad26ffa746
ansonkwokth <87415442+ansonkwokth@users.noreply.github.com>
2025-01-05 18:38:22 +0100

    Update setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="plackett_luce",
+    version="0.1.0",
+    description="Ranking model",
+    author="Anson",
+    author_email="ansonwos@gmail.com",
+    packages=find_packages(where="plackett_luce"),
+    package_dir={"": "plackett_luce"},
+    python_requires=">=3.8",
+    install_requires=[
+        "numpy>=1.26",
+        "torch>=2.5",
+    ],
+)
